757a24ae1de4cc0a3c32828e3176e077fa13fd6f
oumar-dev-app <bourema1@icloud.com>
2026-02-13 02:11:43 +0000

    modification rapide

---
+++ _Components/AboutText.tsx
@@ -0,0 +1,18 @@
+import Container from "./Container";
+
+export default function AboutText() {
+    return(
+        <>
+         <div>
+            <Container>
+                <div>
+                    <div>
+                        <h1>Qui sommes-nous ?</h1>
+                    </div>
+                </div>
+            </Container>
+         </div>
+        </>
+    )
+};
+
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import AboutSlider from "@/_Components/AboutSlider";
+import AboutText from "@/_Components/AboutText";
 
 export default function About (){
     return (
         <div>
             <AboutSlider/>
+            <AboutText/>
         </div>
     )
 }
