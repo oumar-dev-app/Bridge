f858ca94386a24827c9416127d08238ae62c4e8f
oumar-dev-app <bourema1@icloud.com>
2026-02-13 22:35:25 +0000

    modification rapide

--- _Components/WhatsappBtn.tsx
+++ _Components/WhatsappBtn.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { SiWhatsapp } from 'react-icons/si'
 
 function WhatsappBtn() {
-    return (
-        <div className='text-sm font-bold border-2 max-auto border-green-500 
-        rounded-lg p-2 w-50 hover:bg-green-200 hoverEffect shadow-2xl '
-        >
-            <button className='flex items-center text-sm gap-3 cursor-pointer'>
-                <SiWhatsapp size={30} className='text-green-500' /> Contactez-nous
-            </button>
+  const phoneNumber = "+223-78-90-23-89";
+  const message = "Bonjour, je souhaite avoir plus d'informations.";
 
-        </div>
-    )
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+
+  return (
+    <a
+      href={whatsappLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-sm font-bold border-2 mx-auto border-green-500 
+      rounded-lg p-2 w-50 hover:bg-green-200 hoverEffect shadow-2xl 
+      flex items-center gap-3 cursor-pointer"
+    >
+      <SiWhatsapp size={30} className="text-green-500" />
+      Contactez-nous
+    </a>
+  );
 }
 
-export default WhatsappBtn
+export default WhatsappBtn;
