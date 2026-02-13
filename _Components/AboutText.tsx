c662c0e541d941dc077b91bf6e47b2d1f5b05477
oumar-dev-app <bourema1@icloud.com>
2026-02-13 02:25:27 +0000

    modification rapide

--- _Components/AboutText.tsx
+++ _Components/AboutText.tsx
@@ -5,8 +5,8 @@ export default function AboutText() {
         <>
          <div>
             <Container>
-                <div>
-                    <div>
+                <div className="grid grid-cols-1">
+                    <div className="border border-blue-500 rounded-lg bg-white/20">
                         <h1 className="font-bold text-2xl">Qui sommes-nous ?</h1>
                         <p>Bridge est un cabinet de formation spécialisé dans les cours d'Anglais en ligne. 
                             Notre but est de rendre le monde plus petit en servant de pont entre les individus, les cultures. 
@@ -14,7 +14,7 @@ export default function AboutText() {
                             Nous utilisons des méthodes simples et pratiques.</p>
                     </div>
 
-                    <div>
+                    <div className="border border-blue-500 rounded-lg bg-white/20">
                         <h1 className="font-bold text-2xl">Où sommes-nous ?</h1>
                         <p>Kalaban Coura ACI en face de la pharmacie Djouma Barry, Bamako <br />
                             Contact : 223 78 90 23 89</p>
