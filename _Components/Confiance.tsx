c1982936c8eebdceef7bc24c9499dd72d8f58e14
oumar-dev-app <bourema1@icloud.com>
2026-02-14 12:31:38 +0000

    modification rapide

--- _Components/Confiance.tsx
+++ _Components/Confiance.tsx
@@ -1,12 +1,46 @@
-
 import Container from './Container';
-import Image from 'next/image';
+
+
+const ImgSrc = [
+    {
+        id:1,
+        name: "Manda-SISSOKO",
+        disc: "",
+        img: "/Manda-SISSOKO.jpeg"
+    },
+    {
+        id:2,
+        name: "Moussa-Balla-COULIBALY",
+        disc: "",
+        img: "/Moussa-Balla-COULIBALY.jpeg"
+    },
+    {
+        id:3,
+        name: "Mr-YATARA",
+        disc: "",
+        img: "/Logo2"
+    },
+];
 
 function Confiance() {
     return (
         <div className='bg-gray-500 mb-10 p-5'>
             <Container>
-                <h1 className='text-2xl font-semibold text-center text-white mb-8'>Ils nous ont fait confiance !</h1>
+            {ImgSrc.map((item) => {
+                return(
+                    <div key={item.id}>
+                        {item.img}
+                    </div>
+                );
+            })}
+
+
+
+
+
+
+
+                {/* <h1 className='text-2xl font-semibold text-center text-white mb-8'>Ils nous ont fait confiance !</h1>
                 <div className='py-6   grid grid-cols-1 gap-10 m-5 md:grid-cols-2  lg:grid-cols-3'>
                     <div className='space-y-3 text-white flex m-auto flex-col justify-center'>
                         <Image
@@ -24,7 +58,7 @@ function Confiance() {
                             alt='Moussa'
                             width={200}
                             height={200}
-                            priority className='rounded-full'/>
+                            priority className='rounded-full' />
 
                         <p className='text-sm font-semibold'>Moussa Balla COULIBALY</p>
                     </div>
@@ -38,9 +72,9 @@ function Confiance() {
 
                         <p className='text-sm ml-8 font-semibold'>Mr.YATARA</p>
                     </div>
-                 
 
-                </div>
+
+                </div> */}
             </Container>
         </div>
     )
--- app/programme/page.tsx
+++ app/programme/page.tsx
@@ -2,126 +2,128 @@ import Container from "@/_Components/Container"
 
 export default function NosProgramme() {
     return (
-        <div className="m-4 bg-blue-950 p-10 mb-10 rounded-lg ">
-            <Container>
-                <div className="flex  flex-col space-y-10 w-full mb-5 justify-center">
-                    <table className=" border border-white/20 rounded-2xl">
-                        <thead className="flex flex-col justify-center space-y-20 mb-8 text-white items-center">
+        <Container>
+            <div className="m-4 bg-blue-950 p-10 mb-10 rounded-lg ">
+                <Container>
+                    <div className="flex  flex-col space-y-20 w-full mb-5 justify-center">
+                        <table>
+                            <thead className="flex flex-col  space-y-5 mb-8 text-white ">
 
-                            <tr>
-                                <th  className="text-3xl mb-2 font-semibold">Cours individuel</th>
-                            </tr>
-                            <tr>
-                                <th  className=" mb-4 text-center text-md font-normal">Plus d’heures, plus de progrès… et moins d’excuses.</th>
-                            </tr>
-                        </thead>
-                        <tbody className="border text-white">
-                            <tr className="border border-white/20">
-                                <td className="border border-white/20 p-2">Nombre d'heures/semaine</td>
-                                <td className=" border border-white/20 p-2" >Prix</td>
-                            </tr>
-                            <tr className=" border border-white/20 ">
-                                <td className=" border border-white/20 p-2">4 Heures + 1H en ligne</td>
-                                <td className=" border border-white/20 p-2">35000 FCFA</td>
-                            </tr>
-                            <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">6 Heures + 2H en ligne</td>
-                            <td className="border border-white/20 p-2">50.000 FCFA</td>
-                        </tr>
-                        <tr className=" border border-white/20 ">
-                            <td className=" border border-white/20 p-2">8 Heures + 2H en ligne</td>
-                            <td className=" border border-white/20 p-2">65.000 FCFA</td>
+                                <tr>
+                                    <th className="text-3xl mb-2 font-semibold">Cours individuel</th>
+                                </tr>
+                                <tr>
+                                    <th className="text-md font-normal">Plus d’heures, plus de progrès… et moins d’excuses.</th>
+                                </tr>
+                            </thead>
+                            <tbody className=" bg-white rounded-lg text-black">
+                                <tr className="border border-black bg-gray-500 text-white">
+                                    <td className="border border-black p-2">Nombre d'heures/semaine</td>
+                                    <td className=" border border-black p-2" >Prix</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className=" border border-black p-2">4 Heures + 1H en ligne</td>
+                                    <td className=" border border-black p-2">35000 FCFA</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className="border border-black p-2">6 Heures + 2H en ligne</td>
+                                    <td className="border border-black p-2">50.000 FCFA</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className=" border border-black p-2">8 Heures + 2H en ligne</td>
+                                    <td className=" border border-black p-2">65.000 FCFA</td>
 
-                        </tr>
-                    </tbody>
-                </table>
-                <table className=" border border-white/20 rounded-2xl">
-                    <thead className="flex flex-col justify-center space-y-20  mb-8 text-white items-center">
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table>
+                            <thead className="flex flex-col  space-y-5  mb-8 text-white ">
 
-                        <tr>
-                            <th  className="text-3xl  font-semibold">Cours en groupe</th>
-                        </tr>
-                        <tr>
-                            <th  className="text-center mb-4 font-normal">À plusieurs, on comprend mieux… et on rigole un peu aussi.</th>
-                        </tr>
-                    </thead>
-                    <tbody className="bborder border-white/20 text-white">
-                        <tr className="border border-white/20 ">
-                            <td className="border border-white/20 p-2">Nombre d'heures/semaine</td>
-                            <td className="border border-white/20 p-2" >Prix</td>
-                        </tr>
-                        <tr className="border border-white/20 ">
-                            <td className="border border-white/20 p-2">3 Heures</td>
-                            <td className="border border-white/20 p-2">15.000 FCFA</td>
-                        </tr>
-                        <tr className="border border-white/20 ">
-                            <td className="border border-white/20 p-2">4 Heures</td>
-                            <td className="border border-white/20 p-2" >25.000 FCFA</td>
-                        </tr>
-                        <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">6 Heures</td>
-                            <td className="border border-white/20 p-2">35.000 FCFA</td>
+                                <tr>
+                                    <th className="text-3xl  font-semibold">Cours en groupe</th>
+                                </tr>
+                                <tr>
+                                    <th className="mb-4 font-normal">À plusieurs, on comprend mieux… et on rigole un peu aussi.</th>
+                                </tr>
+                            </thead>
+                            <tbody className=" bg-white rounded-lg text-black">
+                                <tr className="border border-black bg-gray-500 text-white">
+                                    <td className="border border-black p-2">Nombre d'heures/semaine</td>
+                                    <td className="border border-black p-2" >Prix</td>
+                                </tr>
+                                <tr className="border border-black ">
+                                    <td className="border border-black p-2">3 Heures</td>
+                                    <td className="border border-black p-2">15.000 FCFA</td>
+                                </tr>
+                                <tr className="border border-black ">
+                                    <td className="border border-black p-2">4 Heures</td>
+                                    <td className="border border-black p-2" >25.000 FCFA</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className="border border-black p-2">6 Heures</td>
+                                    <td className="border border-black p-2">35.000 FCFA</td>
 
-                        </tr>
-                    </tbody>
-                </table>
-                <table className=" border border-white/20 rounded-2xl">
-                    <thead className="flex flex-col justify-center space-y-20  mb-8 text-white items-center">
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table >
+                            <thead className="flex flex-col  space-y-5  mb-8 text-white ">
 
-                        <tr>
-                            <th  className="text-3xl ont-semibold">Cours à domicile</th>
-                        </tr>
-                         <tr>
-                            <th  className="text-center mb-4 font-normal">Vous restez chez vous, on s’occupe du reste (même du savoir).</th>
-                        </tr>
-                    </thead>
-                    <tbody className="border border-white/20 p-2  text-white">
-                        <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">Nombre d'heures/semaine</td>
-                            <td className="border border-white/20 p-2" >Prix</td>
-                        </tr>
-                        <tr className="border border-white/20 ">
-                            <td className="border border-white/20 p-2">4 Heures </td>
-                            <td className="border border-white/20 p-2" >65.000 FCFA</td>
-                        </tr>
-                        <tr className="border border-white/20 ">
-                            <td className="border border-white/20 p-2">3 Heures + 1 Heure en ligne</td>
-                            <td className="border border-white/20 p-2" >50.000 FCFA</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <table className=" border border-white/20 rounded-2xl text-white">
-                    
-                    <thead className="flex flex-col justify-center space-y-20  mb-8 text-white items-center">
-                        <tr>
-                            <th className="text-3xl  font-semibold">Cours en ligne</th>
-                        </tr>
-                          <tr>
-                            <th className="text-center mb-4 font-normal">La formation flexible qui s’adapte à votre emploi du temps.</th>
-                        </tr>
-                    </thead>
-                    <tbody className="bborder border-white/20 p-2 text-white">
-                        <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">Nombre d'heures/semaine</td>
-                            <td className="border border-white/20">Prix</td>
-                        </tr>
-                        <tr className="border border-white/20 p-2">
-                            <td className="border border-white/20 p-2">3 Heures</td>
-                            <td className="border border-white/20 p-2">25.000 FCFA</td>
-                        </tr>
-                        <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">4 Heures</td>
-                            <td className="border border-white/20 p-2" >30.000 FCFA</td>
-                        </tr>
-                        <tr className=" border border-white/20 ">
-                            <td className="border border-white/20 p-2">5 Heures</td>
-                            <td className="border border-white/20 p-2" >40.000 FCFA</td>
+                                <tr>
+                                    <th className="text-3xl ont-semibold">Cours à domicile</th>
+                                </tr>
+                                <tr>
+                                    <th className="mb-4 font-normal">Vous restez chez vous, on s’occupe du reste (même du savoir).</th>
+                                </tr>
+                            </thead>
+                            <tbody className="border bg-white rounded-lg text-black">
+                                <tr className="border border-black bg-gray-500 text-white">
+                                    <td className="border border-black p-2">Nombre d'heures/semaine</td>
+                                    <td className="border border-black p-2" >Prix</td>
+                                </tr>
+                                <tr className="border border-black ">
+                                    <td className="border border-black p-2">4 Heures </td>
+                                    <td className="border border-black p-2" >65.000 FCFA</td>
+                                </tr>
+                                <tr className="border border-black ">
+                                    <td className="border border-black p-2">3 Heures + 1 Heure en ligne</td>
+                                    <td className="border border-black p-2" >50.000 FCFA</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table className="rounded-2xl text-white">
 
-                        </tr>
-                    </tbody>
-                </table>
-        </div>
-            </Container >
-        </div >
+                            <thead className="flex flex-col space-y-5  mb-8 text-white ">
+                                <tr>
+                                    <th className="text-3xl font-semibold">Cours en ligne</th>
+                                </tr>
+                                <tr>
+                                    <th className="mb-4 font-normal">La formation flexible qui s’adapte à votre emploi du temps.</th>
+                                </tr>
+                            </thead>
+                            <tbody className="border bg-white rounded-lg text-black">
+                                <tr className="border border-black bg-gray-500 text-white">
+                                    <td className="border border-black p-2">Nombre d'heures/semaine</td>
+                                    <td className="border border-black p-2">Prix</td>
+                                </tr>
+                                <tr className="border border-black p-2">
+                                    <td className="border border-black p-2">3 Heures</td>
+                                    <td className="border border-black p-2">25.000 FCFA</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className="border border-black p-2">4 Heures</td>
+                                    <td className="border border-black p-2" >30.000 FCFA</td>
+                                </tr>
+                                <tr className=" border border-black ">
+                                    <td className="border border-black p-2">5 Heures</td>
+                                    <td className="border border-black p-2" >40.000 FCFA</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </Container >
+            </div >
+        </Container>
     )
 }
