5e6a2f3fe3086bc4f0aac5608def685dad43acfd
oumar-dev-app <bourema1@icloud.com>
2026-02-18 14:06:25 +0000

    modification rapide

---
+++ _Components/Carousel.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Image from 'next/image';
+
+const sliders = [
+  { id: 1, 
+    srcImage: '/Manda-SISSOKO.jpeg', 
+    title: 'Etidiant1', 
+    dscr: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nobis tempore blanditiis iure consectetur laudantium! " },
+  { id: 2, 
+    srcImage: '/Moussa-Balla-COULIBALY.jpeg', 
+    title: 'Etidiant2', 
+    dscr: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nobis tempore blanditiis iure consectetur laudantium! " },
+  { id: 3, 
+    srcImage: '/Mr-YATARA.jpeg', 
+    title: 'Etidiant3', 
+    dscr: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nobis tempore blanditiis iure consectetur laudantium! " },
+  { id: 4, 
+    srcImage: '/Manda-SISSOKO.jpeg', 
+    title: 'Etidiant4', 
+    dscr: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo nobis tempore blanditiis iure consectetur laudantium! " },
+]
+export default function Carousel() {
+
+
+  return (
+    <div className=" w-full max-w-7xl mx-auto">
+      <h1 className='text-center text-2xl font-semibold text-white'>Il nous fait confiance !</h1>
+      <div className='overflow-hidden'>
+        <div className='py-6  grid grid-cols-1 gap-6 m-5 md:grid-cols-2  lg:grid-cols-4'>
+          {sliders.map((item) => {
+            return(
+              <div className='bg-white w-75 rounded-lg m-2  space-y-5'>
+              <Image
+              src={item.srcImage}
+              alt='image'
+              height={300}
+              width={300}
+              priority
+              className=' rounded-t-lg m-auto'
+              />
+              <div className='space-y-5 m-2'>
+                <h1 className='text-md font-bold'>{item.title}</h1>
+                <p className='text-sm '>{item.dscr}</p>
+              </div>
+              </div>
+            
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
--- _Components/Confiance.tsx
+++ _Components/Confiance.tsx
@@ -1,82 +1,46 @@
-import Container from './Container';
+import Carousel from "./Carousel";
+import Container from "./Container";
 
 
-const ImgSrc = [
+const imgs = [
     {
-        id:1,
-        name: "Manda-SISSOKO",
-        disc: "",
-        img: "/Manda-SISSOKO.jpeg"
+        id: 1,
+        name: "etudiant",
+        dscr: "Un etudiant déja finit",
+        src: "/presentiel.jpeg"
+    },
+
+    {
+        id: 2,
+        name: "etudiant",
+        dscr: "Un etudiant déja finit",
+        src: "/presentiel.jpeg"
     },
     {
-        id:2,
-        name: "Moussa-Balla-COULIBALY",
-        disc: "",
-        img: "/Moussa-Balla-COULIBALY.jpeg"
+        id: 3,
+        name: "etudiant",
+        dscr: "Un etudiant déja finit",
+        src: "/presentiel.jpeg"
     },
     {
-        id:3,
-        name: "Mr-YATARA",
-        disc: "",
-        img: "/Logo2"
+        id: 4,
+        name: "etudiant",
+        dscr: "Un etudiant déja finit",
+        src: "/presentiel.jpeg"
     },
-];
+]
+
+
 
 function Confiance() {
     return (
-        <div className='bg-gray-500 mb-10 p-5'>
+        <div className="bg-gray-400 p-5 ">
             <Container>
-            {ImgSrc.map((item) => {
-                return(
-                    <div key={item.id}>
-                        {item.img}
-                    </div>
-                );
-            })}
-
-
-
-
-
-
-
-                {/* <h1 className='text-2xl font-semibold text-center text-white mb-8'>Ils nous ont fait confiance !</h1>
-                <div className='py-6   grid grid-cols-1 gap-10 m-5 md:grid-cols-2  lg:grid-cols-3'>
-                    <div className='space-y-3 text-white flex m-auto flex-col justify-center'>
-                        <Image
-                            src={"/Manda-SISSOKO.jpeg"}
-                            alt='Manda'
-                            width={200}
-                            height={200}
-                            priority className='rounded-full' />
-
-                        <p className='text-sm font-semibold'>Manda SISSOKO</p>
-                    </div>
-                    <div className='space-y-3 text-white flex m-auto flex-col justify-center'>
-                        <Image
-                            src={"/Moussa-Balla-COULIBALY.jpeg"}
-                            alt='Moussa'
-                            width={200}
-                            height={200}
-                            priority className='rounded-full' />
-
-                        <p className='text-sm font-semibold'>Moussa Balla COULIBALY</p>
-                    </div>
-                    <div className='space-y-3 text-white flex m-auto flex-col justify-center'>
-                        <Image
-                            src={"/Mr-YATARA.jpeg"}
-                            alt='yattara'
-                            width={200}
-                            height={200}
-                            priority className='rounded-full' />
-
-                        <p className='text-sm ml-8 font-semibold'>Mr.YATARA</p>
-                    </div>
-
-
-                </div> */}
+              <Carousel/>
             </Container>
         </div>
+
+
     )
 }
 
